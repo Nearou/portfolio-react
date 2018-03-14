bcc1ab5ce7b21f2a5f90c27b53d0ed29c41fe449
Nearou <baptiste.deruelle@viacesi.fr>
2018-03-15 00:30:34 +0100

    first commit

---
+++ pages/prout.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+import Page from '../components/page'
+import Layout from '../components/layout'
+
+export default class extends Page {
+  render() {
+    return (
+      <div>prout</div>
+    )
+  }
+}
